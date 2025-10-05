cdea6c3a67c45e6b03bf5ae9f5bdc50bf48c13df
pastel67 <chykjoun@gmail.com>
2025-10-05 15:07:51 +0900

    loadStudentData로  학생정보 연결 && 인풋 입력할 수 있는 기능 구현

--- bin/assignment2.dart
+++ bin/assignment2.dart
@@ -1,6 +1,14 @@
+import 'dart:io';
 import 'package:assignment2/loadStudentData.dart';
 import 'package:assignment2/score.dart';
 
-void main () {
-   
+void main(List<String> arguments) {
+  List<StudentScore> studentsInfo = [];
+
+  studentsInfo = loadStudentData('students.txt');
+
+  while (true) {
+    stdout.write('어떤 학생의 통계를 확인하시겠습니까? ');
+    String? input = stdin.readLineSync();
+  }
 }
--- lib/loadStudentData.dart
+++ lib/loadStudentData.dart
@@ -1,26 +1,25 @@
 import 'dart:io';
-
 import 'package:assignment2/score.dart';
 
-void LoadStudentData(String filePath) {
+List<StudentScore> loadStudentData(String filePath) {
+  List<StudentScore> studentsInfo = [];
+  
   try {
     final file = File(filePath);
     final List<String> lines = file.readAsLinesSync();
 
-    List<StudentScore> studentsInfo = [];
-
     for (var line in lines) {
-      final List<dynamic>parts = line.split(',');
+      final List<dynamic> parts = line.split(',');
       if (parts.length != 2) throw FormatException('잘못된 데이터 형식: $line');
 
       String student = parts[0].trim();
       int score = int.parse(parts[1]);
-    
+
       studentsInfo.add(StudentScore(student, score));
     }
   } catch (e) {
     print("학생 데이터를 불러오는 데 실패했습니다: $e");
     exit(1);
   }
-  return;
+  return studentsInfo;
 }
